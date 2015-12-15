cdf712449795ad2454eedb4b9497412073906222
illfang <kratzer.kevin@gmail.com>
2015-12-15 11:06:13 +0900

    untabifiy

--- test/test_xml_validator.js
+++ test/test_xml_validator.js
@@ -4,47 +4,47 @@ var grunt = require('grunt');
 var exec = require('child_process').exec;
 
 /*
-	======== A Handy Little Nodeunit Reference ========
-	https://github.com/caolan/nodeunit
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
 
-	Test methods:
-		test.expect(numAssertions)
-		test.done()
-	Test assertions:
-		test.ok(value, [message])
-		test.equal(actual, expected, [message])
-		test.notEqual(actual, expected, [message])
-		test.deepEqual(actual, expected, [message])
-		test.notDeepEqual(actual, expected, [message])
-		test.strictEqual(actual, expected, [message])
-		test.notStrictEqual(actual, expected, [message])
-		test.throws(block, [error], [message])
-		test.doesNotThrow(block, [error], [message])
-		test.ifError(value)
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
 */
 
 exports.xml_validator = {
-	valid: function(test) {
+  valid: function(test) {
 
-	test.expect(1);
+  test.expect(1);
 
-	exec('grunt xml_validator:valid', function(error, stdout) { 
-		var success = error === null;
-		test.ok(success, 'Valid xml files are considered valid');
-		test.done();
-	});
+  exec('grunt xml_validator:valid', function(error, stdout) { 
+    var success = error === null;
+    test.ok(success, 'Valid xml files are considered valid');
+    test.done();
+  });
 
-	},
-	invalid: function(test) {
+  },
+  invalid: function(test) {
 
-		test.expect(1);
+    test.expect(1);
 
-		exec('grunt xml_validator:invalid', function(error, stdout) { 
-			var success = error !== null;
-			test.ok(success, 'Invalid xml files are not considered invalid');
-			test.done();
+    exec('grunt xml_validator:invalid', function(error, stdout) { 
+      var success = error !== null;
+      test.ok(success, 'Invalid xml files are not considered invalid');
+      test.done();
 
-		});
+    });
 
-	}
+  }
 };
